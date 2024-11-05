60a31c95c667a3ce5dc407b24961bac838640f18
caiozanetoni <cmzanetoni@gmail.com>
2024-11-05 17:06:16 -0300

    Exemplos de comentários

--- fundamentos/src/components/FirstComponent.jsx
+++ fundamentos/src/components/FirstComponent.jsx
@@ -1,6 +1,16 @@
+// Exemplo de comentário
+
 const FirstComponent = () => {
+    // essa função faz isso
+
+    /*
+    comentário de
+    múltiplas linhas
+     */
+
     return (
         <div>
+            {/* comentário em interpolação */}
             <h1>Meu Primeiro Componente</h1>
             <p className="teste"></p>
         </div>
