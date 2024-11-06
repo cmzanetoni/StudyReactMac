51b058d562dbf571ab832b7992a5543ed9bb97db
caiozanetoni <cmzanetoni@gmail.com>
2024-11-06 06:27:08 -0300

    States  useState retirando warning ao renderizar os valores em array em uma lista pois não existia um ID
     Foi feito com ID e classe

--- 3_AvancandoEmReact/avancando/src/components/ListRender.jsx
+++ 3_AvancandoEmReact/avancando/src/components/ListRender.jsx
@@ -1,13 +1,23 @@
 import {useState} from "react";
 
 const ListRender = () => {
-        const [list] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
+    const [list] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
+    const [users] = useState([
+        {id: 1, name: "Matheus", age: 31},
+        {id: 2, name: "João", age: 28},
+        {id: 55, name: "Pedro", age: 44},
+    ]);
 
     return (
         <div>
             <ul>
                 {list.map((item) => (
                     <li>{item}</li>
+                ))}
+            </ul>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name} - {user.age}</li>
                 ))}
             </ul>
         </div>
