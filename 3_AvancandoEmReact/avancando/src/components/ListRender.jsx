5bcae4ac6edb6fbc85fb89789e7a76d449160e02
caiozanetoni <cmzanetoni@gmail.com>
2024-11-07 05:30:31 -0300

    Hook States - Previous States
    Foi utilizado o Previous States pegando estados anteriores,
    Com isso foi gerado valor aleatório para deletar um nome cadastrado na lista

--- 3_AvancandoEmReact/avancando/src/components/ListRender.jsx
+++ 3_AvancandoEmReact/avancando/src/components/ListRender.jsx
@@ -2,11 +2,33 @@ import {useState} from "react";
 
 const ListRender = () => {
     const [list] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
-    const [users] = useState([
+    const [users, setUsers] = useState([
         {id: 1, name: "Matheus", age: 31},
         {id: 2, name: "João", age: 28},
         {id: 55, name: "Pedro", age: 44},
     ]);
+
+    console.log("users:");
+    console.log(users);
+    console.log("setUsers:");
+    console.log(setUsers);
+
+    const deleteRandom = () => {
+        // Gerar inteiro random entre os que estão no array users
+        // Math.random(): gera um número aleatório (float)
+        // Math.floor(x): arredonda o número para inteiro
+        // * maxValue: limita o valor máximo
+        // * (maxValue - minValue) + minValue: limita o valor máximo e mínimo
+        const randomNumber = Math.floor(Math.random() * (users.length - 1) + 1);
+
+        console.log("randomNumber: " + randomNumber);
+
+        setUsers((prevUsers) => {
+            console.log("prevUsers:");
+            console.log(prevUsers);
+            return prevUsers.filter(user => user.id !== randomNumber);
+        })
+    }
 
     return (
         <div>
@@ -20,6 +42,7 @@ const ListRender = () => {
                     <li key={user.id}>{user.name} - {user.age}</li>
                 ))}
             </ul>
+            <button onClick={deleteRandom}>Delete random user</button>
         </div>
     )
 }
