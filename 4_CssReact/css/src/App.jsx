b44d64603f8f96eeef380018e0dae18312c113ff
caiozanetoni <cmzanetoni@gmail.com>
2024-11-13 06:33:02 -0300

    Utilizando CSS no componente:
    - CSS Inline Dinânimco com Classes e IF ternário

--- 4_CssReact/css/src/App.jsx
+++ 4_CssReact/css/src/App.jsx
@@ -10,6 +10,8 @@ function App() {
   const n = 15;
   const [name] = useState("Caio");
 
+  const redTitle = true;
+
   return (
       <>
           {/* CSS global */}
@@ -18,12 +20,15 @@ function App() {
           <MyComponent/>
           <p>Este parágrafo é do App.js</p>
           {/* Inline CSS */}
-          <p style={{color: "blue", padding: "25px", color: "blue", padding: "25 px", borderTop: "2px solid red"}}>Este
-              elemento foi estilizado de forma inline</p>
+          <p style={{color: "blue", padding: "25px", borderTop: "2px solid red"}}>
+              Este elemento foi estilizado de forma inline
+          </p>
           {/* CSS Inline dinâmico */}
           <h2 style={n < 10 ? {color: "purple"} : {color: "pink"}}>CSS Dinâmico</h2>
           <h2 style={n > 10 ? {color: "purple"} : {color: "pink"}}>CSS Dinâmico</h2>
           <h2 style={name === "Caio" ? {color: "green", backgroundColor: "#000"} : null}>Teste Nome</h2>
+          {/* Classe dinämica */}
+          <h2 className={redTitle ? "red-title" : "title"}>Este título vai ter classe dinâmica</h2>
       </>
   )
 }
