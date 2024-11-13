5453612077a5a0e62c99abf1c996b7667fb8e13c
caiozanetoni <cmzanetoni@gmail.com>
2024-11-13 06:22:12 -0300

    Utilizando CSS no componente:
    - CSS Inline Dinânimco com IF ternário

--- 4_CssReact/css/src/App.jsx
+++ 4_CssReact/css/src/App.jsx
@@ -7,16 +7,24 @@ import MyComponent from "./Components/MyComponent.jsx";
 function App() {
   const [count, setCount] = useState(0)
 
+  const n = 15;
+  const [name] = useState("Caio");
+
   return (
-    <>
-      {/* CSS global */}
-      <h1>React com CSS</h1>
-      {/* CSS de componente */}
-      <MyComponent />
-      <p>Este parágrafo é do App.js</p>
-      {/* Inline CSS */}
-      <p style={{ color: "blue", padding: "25px", color: "blue", padding: "25 px", borderTop: "2px solid red": "2px solid red" }}>Este elemento foi estilizado de forma inline</p>
-    </>
+      <>
+          {/* CSS global */}
+          <h1>React com CSS</h1>
+          {/* CSS de componente */}
+          <MyComponent/>
+          <p>Este parágrafo é do App.js</p>
+          {/* Inline CSS */}
+          <p style={{color: "blue", padding: "25px", color: "blue", padding: "25 px", borderTop: "2px solid red"}}>Este
+              elemento foi estilizado de forma inline</p>
+          {/* CSS Inline dinâmico */}
+          <h2 style={n < 10 ? {color: "purple"} : {color: "pink"}}>CSS Dinâmico</h2>
+          <h2 style={n > 10 ? {color: "purple"} : {color: "pink"}}>CSS Dinâmico</h2>
+          <h2 style={name === "Caio" ? {color: "green", backgroundColor: "#000"} : null}>Teste Nome</h2>
+      </>
   )
 }
 
