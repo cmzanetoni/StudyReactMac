32f41f8cc4b292ae4e4293813a7522b0e9d1d076
caiozanetoni <cmzanetoni@gmail.com>
2024-11-15 17:08:56 -0300

    Gerenciamento de dados
    Controlled inputs - recurso que permite mais flexibilidade nos fomrs React
    Precisamos itualar o valor ao state
    
    Comum em formulários de edição que vem do back-end e preencher o input mais facilmente

--- 5_FormEmReact/form/src/components/MyForm.jsx
+++ 5_FormEmReact/form/src/components/MyForm.jsx
@@ -1,10 +1,12 @@
 import './MyForm.css'
 import {useState} from "react";
 
-const MyForm = () => {
+const MyForm = ({user}) => {
+    // 6 - Controlled do input
+
     // 3 - Gerenciamento de dados
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
+    const [name, setName] = useState(user ? user.name : "")
+    const [email, setEmail] = useState(user ? user.email : "")
 
     const handleName = (e) => {
         setName(e.target.value);
@@ -26,13 +28,16 @@ const MyForm = () => {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} />
+                    <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} value={name} />
                 </div>
                 {/* 2 - lavel envolvendo input */}
                 <label>
                     <span>E-mail</span>
                     {/* 4 - simplificação de manipulação de state */}
-                    <input type="email" name="email" placeholder="Digite o seu e-mail" onChange={(e) => setEmail(e.target.value)} />
+                    <input type="email" name="email" placeholder="Digite o seu e-mail"
+                           onChange={(e) => setEmail(e.target.value)}
+                           value={email}
+                    />
                 </label>
                 <input type="submit" value="Enviar" />
             </form>
