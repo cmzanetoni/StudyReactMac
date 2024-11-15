e889f95ebd283f3a49e074dd82b63987c89331fc
caiozanetoni <cmzanetoni@gmail.com>
2024-11-15 16:41:41 -0300

    Gerenciamento de dados de input criando a função inline no onChange
    Modo mais difícil de manutenção pois é gerados para todos inputs, mas se for apenas para pegar dados é mais fácil

--- 5_FormEmReact/form/src/components/MyForm.jsx
+++ 5_FormEmReact/form/src/components/MyForm.jsx
@@ -11,6 +11,7 @@ const MyForm = () => {
     }
 
     console.log(name);
+    console.log(email);
 
     return (
         <div>
@@ -23,7 +24,7 @@ const MyForm = () => {
                 {/* 2 - lavel envolvendo input */}
                 <label>
                     <span>E-mail</span>
-                    <input type="email" name="email" placeholder="Digite o seu e-mail"/>
+                    <input type="email" name="email" placeholder="Digite o seu e-mail" onChange={(e) => setEmail(e.target.value)} />
                 </label>
                 <input type="submit" value="Enviar" />
             </form>
