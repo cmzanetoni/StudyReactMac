80db7a1777763c0d661a927ebc585fc2a4c86f61
caiozanetoni <cmzanetoni@gmail.com>
2024-11-16 15:02:26 -0300

    Css para o botão

--- 6_SecretWord/SecretWord/src/App.jsx
+++ 6_SecretWord/SecretWord/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import StartScreen from "./components/StartScreen.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-        <h2>Secret Word</h2>
+        <StartScreen />
     </div>
   )
 }
