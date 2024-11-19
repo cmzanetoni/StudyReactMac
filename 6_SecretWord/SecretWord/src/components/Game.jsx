2472ec06b105c7f41d5d4b037506f5d0bdd1ba63
caiozanetoni <cmzanetoni@gmail.com>
2024-11-19 05:36:44 -0300

    colocados elementos da tela do jogo (game)

--- 6_SecretWord/SecretWord/src/components/Game.jsx
+++ 6_SecretWord/SecretWord/src/components/Game.jsx
@@ -2,9 +2,31 @@ import "./Game.css"
 
 const Game = ( {verityLetter}) => {
     return (
-        <div>
-            <h1>Game</h1>
-            <button onClick={verityLetter}>Finalizar jogo</button>
+        <div className="game">
+            <p className="point">
+                <span>Pontuação: 000</span>
+            </p>
+            <h1>Adivinhe a palavra:</h1>
+            <h3 className="tip">
+                Dica sobre a palavra: <span>Dica...</span>
+            </h3>
+            <div className="wordContainer">
+                <span className="letter">A</span>
+                <span className="blankSquare">B</span>
+            </div>
+            <div className="letterContainer">
+                <p>Tente advinhar uma letra da palavra:</p>
+                <form>
+                    <input type="text" name="letter" maxLength="1" required/>
+                    <button>Jogar!</button>
+                </form>
+            </div>
+            <div className="wrongLettersContainer">
+                <p>Letras já utilizadas</p>
+                <span>a, </span>
+                <span>b, </span>
+            </div>
+            <button onClick={verityLetter} >Finalizar jogo</button>
         </div>
     )
 }
