1bb8087691a847776134bec6dcbfa154a93fe83c
caiozanetoni <cmzanetoni@gmail.com>
2024-11-19 05:21:28 -0300

    Implementado importação das palavras, sorteadas por categoria e divida em letras

--- 6_SecretWord/SecretWord/src/App.jsx
+++ 6_SecretWord/SecretWord/src/App.jsx
@@ -20,10 +20,45 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList);
+  const [pickedWord, setPickedWord] = useState("")
+  const [picketCategory, setPicketCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+
+  const pickWordAndCategory = () => {
+      // pick a random category
+      const categories = Object.keys(words)
+      //console.log(categories.length)
+      //console.log(Object.keys(categories).length)
+      //console.log(Math.floor(Math.random() * categories.length))
+      //console.log(Math.floor(Math.random() * Object.keys(categories).length))
+      const category = categories[Math.floor(Math.random() * categories.length)]
+      console.log(category)
+
+      // pick a random word
+      const word = words[category][Math.floor(Math.random() * words[category].length)]
+      console.log(word)
+
+      return { word, category }
+  }
 
   // starts the scret word game
   const startGame = () => {
-      setGameStage(stages[1].name)
+      // pick word and pick category
+      const { word, category } = pickWordAndCategory();
+
+      // create an array of letters
+      let wordLetters = word.toLowerCase().split("");
+
+      console.log(word, category);
+      console.log(wordLetters);
+
+      // fill state
+      setPickedWord(word);
+      setPicketCategory(category);
+      setLetters(wordLetters);
+
+      setGameStage(stages[1].name);
   }
 
   // process the letter input
