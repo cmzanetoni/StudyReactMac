548fe0b3f9606d781865613fb7c7e2a007a50c76
caiozanetoni <cmzanetoni@gmail.com>
2024-12-03 06:23:01 -0300

    React Router - Carregamento de dado individual
    utilizando url com o id do produto utilizando o fetch

--- 8_ReactRouter/reactrouter/src/pages/Product.jsx
+++ 8_ReactRouter/reactrouter/src/pages/Product.jsx
@@ -1,12 +1,25 @@
-import { useParams } from "react-router-dom";
+import {data, useParams} from "react-router-dom";
+import {useFetch} from "../hooks/useFetch.jsx";
 
 const Product = () => {
   // 4 - roda dinamica
   const {id} = useParams();
 
+  // 5 - carregamento dado individual
+  const url = "http://localhost:3000/products/" + id;
+  const {data: product, loading, error} = useFetch(url);
+
   return (
     <>
       <p>Id do produto: {id}</p>
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R${product.price}</p>
+        </div>
+      )}
     </>
   )
 }
