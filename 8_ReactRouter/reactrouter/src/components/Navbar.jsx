6ed722fed0258d18df05d4e60b5ba2b45ebd318c
caiozanetoni <cmzanetoni@gmail.com>
2024-12-05 06:01:37 -0300

    React Router - Link ativo
    NavLink - acesso ao isActive, podendo saber a classe ativa e ativar uma classe se está na rota com o atributo to

--- 8_ReactRouter/reactrouter/src/components/Navbar.jsx
+++ 8_ReactRouter/reactrouter/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 // 2 - links com react router
 import "./Navbar.css"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <nav>
+      {/* Menu antigo
       <Link to="/">Home</Link>
       <Link to="/about">Sobre</Link>
+      */}
+      {/* se quiser definir classe com o isActive, mas teria que fazer isso em todos e atualmente tem nativo
+      <NavLink to="/" className={({isActive}) => (isActive ? 'esta-ativo' : 'nao-ativo')}>Home</NavLink>*/}
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">Sobre</NavLink>
     </nav>
   )
 }
