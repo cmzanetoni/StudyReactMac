d89b120a01dc8a1b68e7c53a7d5b0124a034139e
caiozanetoni <cmzanetoni@gmail.com>
2024-12-09 06:27:46 -0300

    Context API
    criadas 3 páginas e navbar (menu)

--- 9_ContextApi/contextapi/src/App.jsx
+++ 9_ContextApi/contextapi/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Navbar from './components/Navbar'
+
+import About from './pages/About'
+import Home from './pages/Home'
+import Info from './pages/Info'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/info" element={<Info />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
