770952312b11ffdd05d806be1ae25c1fdfc5e744
caiozanetoni <cmzanetoni@gmail.com>
2025-11-22 16:03:49 -0300

    Refatorando o context para usar com hook

--- 9_ContextApi/contextapi/src/pages/Home.jsx
+++ 9_ContextApi/contextapi/src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import {useContext} from "react";
-import { CounterContext } from "../context/CounterContext";
-import ChangeCounter from "../components/ChangeCounter.jsx";
+// import {useContext} from "react";
+// import { CounterContext } from "../context/CounterContext";
+import ChangeCounter from "../components/ChangeCounter";
+
+// 4 refatorando com hook
+import { useCounterContext} from "../hooks/useCounterContext.jsx";
 
 const Home = () => {
-  const {counter} = useContext(CounterContext);
+  // const {counter} = useContext(CounterContext);
+  const {counter} = useCounterContext();
 
   return (
     <div>
--- 9_ContextApi/contextapi/src/pages/Info.jsx
+++ 9_ContextApi/contextapi/src/pages/Info.jsx
@@ -1,7 +1,14 @@
+import ChangeCounter from "../components/ChangeCounter";
+import {useCounterContext} from "../hooks/useCounterContext";
+
 const Info = () => {
+  const {counter} = useCounterContext();
+
   return (
     <div>
       <h1>Info</h1>
+      <p>Valor do contador: {counter}</p>
+      <ChangeCounter />
     </div>
   )
 }
