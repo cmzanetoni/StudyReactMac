55d3d057e6bdd738a797259a3e282a70827127c6
caiozanetoni <cmzanetoni@gmail.com>
2025-11-23 20:17:23 -0300

    Configurando o React Router: Criadas paginas home, about e as rotas delas

--- 10_Miniblog/miniblog/src/App.jsx
+++ 10_Miniblog/miniblog/src/App.jsx
@@ -1,30 +1,22 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Pages
+import Home from "./pages/Home/Home.jsx";
+import About from "./pages/About/About.jsx";
 
 function App() {
 
   return (
     <>
-      <h1>MiniBlog</h1>
-      {/*<div>*/}
-      {/*  <a href="https://vite.dev" target="_blank">*/}
-      {/*    <img src={viteLogo} className="logo" alt="Vite logo" />*/}
-      {/*  </a>*/}
-      {/*  <a href="https://react.dev" target="_blank">*/}
-      {/*    <img src={reactLogo} className="logo react" alt="React logo" />*/}
-      {/*  </a>*/}
-      {/*</div>*/}
-      {/*<h1>Vite + React</h1>*/}
-      {/*<div className="card">*/}
-      {/*  <button onClick={() => setCount((count) => count + 1)}>*/}
-      {/*    count is {count}*/}
-      {/*  </button>*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.jsx</code> and save to test HMR*/}
-      {/*  </p>*/}
-      {/*</div>*/}
-      {/*<p className="read-the-docs">*/}
-      {/*  Click on the Vite and React logos to learn more*/}
-      {/*</p>*/}
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
