e44acf04bded3ab477719eaba6dc90c76e0ff2c9
caiozanetoni <cmzanetoni@gmail.com>
2025-11-23 19:54:21 -0300

    Integrando Firebase:
    Copiado o arquivo de configuração do firebase no site (engrenagem do app) e adicionada as linhas db e export

---
+++ 10_Miniblog/miniblog/firebase/config.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbAYZ1aTsqEvRfImO2YbKLtUvc2DdWdEY",
+  authDomain: "miniblog-511f9.firebaseapp.com",
+  projectId: "miniblog-511f9",
+  storageBucket: "miniblog-511f9.firebasestorage.app",
+  messagingSenderId: "969789653188",
+  appId: "1:969789653188:web:9e8412c6d98ddde9748991"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
