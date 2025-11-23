f839594c4bf458af8c3d2e8a6a0171f0b29c0049
caiozanetoni <cmzanetoni@gmail.com>
2025-11-22 21:55:18 -0300

    Criando dontexto mais complexo: Utilizando hook useReducer, é como o useState mas pode controlar mais dados. Tem diferentes ações com um swtich

---
+++ 9_ContextApi/contextapi/src/hooks/useTitleColorContext.jsx
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import {TitleColorContext} from "../context/TitleColorContext.jsx";
+
+export const useTitleColorContext = () => {
+  const context = useContext(TitleColorContext);
+
+  if (!context) {
+    console.log("Context não encontrado");
+  }
+
+  return context;
+}
--- 9_ContextApi/contextapi/src/pages/Home.jsx
+++ 9_ContextApi/contextapi/src/pages/Home.jsx
@@ -2,16 +2,22 @@
 // import { CounterContext } from "../context/CounterContext";
 import ChangeCounter from "../components/ChangeCounter";
 
-// 4 refatorando com hook
+// 4 - Rfatorando com hook
 import { useCounterContext} from "../hooks/useCounterContext.jsx";
+
+// 5 - Context mais complexo
+import {useTitleColorContext} from "../hooks/useTitleColorContext.jsx";
 
 const Home = () => {
   // const {counter} = useContext(CounterContext);
   const {counter} = useCounterContext();
 
+  // 5 - Context mais complexo
+  const { color } = useTitleColorContext();
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1 style={{ color: color }}>Home</h1>
       <p>Valor do contador: {counter}</p>
       {/* 3 - Alterando valor contexto */}
       <ChangeCounter />
