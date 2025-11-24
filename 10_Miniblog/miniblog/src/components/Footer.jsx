7ee623fcc73ff2c7775bc06db977e9657641439b
caiozanetoni <cmzanetoni@gmail.com>
2025-11-23 21:33:31 -0300

    CSS navbar e footer

--- 10_Miniblog/miniblog/src/components/Footer.jsx
+++ 10_Miniblog/miniblog/src/components/Footer.jsx
@@ -1,8 +1,8 @@
-import style from "./Footer.module.css";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Escreva sobre o que você tem interesse!</h3>
       <p>Mini Blog &copy; 2025</p>
     </footer>
