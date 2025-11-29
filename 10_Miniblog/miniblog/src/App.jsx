94f822be8e97c3368f2f3ff9ba773502a66def34
caiozanetoni <cmzanetoni@gmail.com>
2025-11-29 15:14:52 -0300

    Criando formulario de cadastro

--- 10_Miniblog/miniblog/src/App.jsx
+++ 10_Miniblog/miniblog/src/App.jsx
@@ -6,6 +6,8 @@ import Home from "./pages/Home/Home.jsx";
 import About from "./pages/About/About.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Register from "./pages/Register/Register.jsx";
 
 function App() {
 
@@ -16,7 +18,9 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
         <Footer />
